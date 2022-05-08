61344755a42d1e4425446b3016096070d9903133
Ihor Melnyk <92684157+Ihor-Melnyk@users.noreply.github.com>
2022-05-09 00:41:26 +0300

    addhtml

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const containerGrig = document.querySelector('.gallery');
+const markup = createImages(galleryItems);
+containerGrig.insertAdjacentHTML('beforeend', markup);
 
-console.log(galleryItems);
+function createImages(galleryItems) {
+    return galleryItems.map(({ original, preview, description }) => {
+        return `
+            <div class="gallery__item">
+              <a class="gallery__link" href="${original}">
+                <img
+                  class="gallery__image"
+                  src="${preview}"
+                  data-source="large-image.jpg"
+                  alt="${description}"
+                />
+              </a>
+            </div>
+            `
+    }).join('');
+}
+
+containerGrig.addEventListener('click', onImageClick);
+function onImageClick(e) {
+    if (!e.target.classList.containerGrig('gallery__image')) {
+        return
+    }
+    console.log(e.target);
+} 
